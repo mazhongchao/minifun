fa7688eb41e603de2aea7784c66a815efda9fb48
mazhongchao <mazhongchao@gmail.com>
2023-01-18 15:04:41 +0800

    class MFConsole test file

--- test/MFConsoleTest.php
+++ test/MFConsoleTest.php
@@ -2,48 +2,75 @@
 require "../vendor/autoload.php";
 
 $console_types = [
-    'INFO' => 'info', 
-    'NOTICE' => 'notice', 
-    'WARNING' => 'warning',
-    'SUCCESS' => 'success',
-    'ERROR' => 'error', 
-    'Blue' => 'blue', 
-    'Green' => 'green', 
-    'Red' => 'red',
-    'White' => 'white',
-    'Yellow'  => 'yellow'
+    'emergency' => 'EMERGENCY',
+    'alert' => 'ALERT',
+    'critical' => 'CRITICAL',
+    'error' => 'ERROR',
+    'warning' => 'WARNING',
+    'notice' => 'NOTICE',
+    'info' => 'INFO',
+    'debug' => 'DEBUG',
+    'success' => 'SUCCESS',
+    'common' => 'COMMON',
+    'black' => 'Black',
+    'red' => 'Red',
+    'green' => 'Green',
+    'yellow'  => 'Yellow',
+    'blue' => 'Blue',
+    'purple' => 'Purple',
+    'cyan' => 'Cyan',
+    'white' => 'White',
+    'gray' => 'Gray'
 ];
 
 $text_types = [
-    'INFO' => 'info',
-    'NOTICE' => 'notice',
-    'WARNING' => 'warning',
-    'ERROR' => 'error',
-    'SUCCESS' => 'success',
-    'Black' => 'black',
-    'Blue' => 'blue', 
-    'Green' => 'green',
-    'Red' => 'red',
-    'Yellow'  => 'yellow'
+    'emergency' => 'EMERGENCY',
+    'alert' => 'ALERT',
+    'critical' => 'CRITICAL',
+    'error' => 'ERROR',
+    'warning' => 'WARNING',
+    'notice' => 'NOTICE',
+    'info' => 'INFO',
+    'debug' => 'DEBUG',
+    'success' => 'SUCCESS',
+    'common' => 'COMMON',
+    'black' => 'Black',
+    'red' => 'Red',
+    'green' => 'Green',
+    'yellow'  => 'Yellow',
+    'blue' => 'Blue',
+    'purple' => 'Purple',
+    'cyan' => 'Cyan',
+    'white' => 'White',
+    'gray' => 'Gray',
+    'black-d' => 'Black',
+    'red-d' => 'Red',
+    'green-d' => 'Green',
+    'yellow-d'  => 'Yellow',
+    'blue-d' => 'Blue',
+    'purple-d' => 'Purple',
+    'cyan-d' => 'Cyan',
+    'white-d' => 'White',
+    'gray-d' => 'Gray'
 ];
 
 if (strpos(php_sapi_name(), 'cli') !== false) {
-    foreach ($console_types as $key => $value) {
-        MFConsole::log("test MFConsole::log()", $value, "{$key}: ");
+    foreach ($console_types as $type => $pre_text) {
+        MFConsole::log("test MFConsole::log()", $type, "{$pre_text}: ");
     }
     
-    MFConsole::log("common info");
+    MFConsole::log("plain text");
     
     $arr = ['foo', 'foo'=>'bar'];
     MFConsole::log($arr);
 }
 else {
-    foreach ($text_types as $key => $value) {
-        MFConsole::info("test MFConsole::info()", $value, "{$key}: ");
+    foreach ($text_types as $type => $pre_text) {
+        MFConsole::info("test MFConsole::info()", $type, "{$pre_text}: ");
     }
     
-    MFConsole::info("common info");
+    MFConsole::info("plain text");
     
     $arr = ['foo', 'foo'=>'bar'];
-    MFConsole::info($arr);
+    MFConsole::info($arr, '', 'array dump');
 }
