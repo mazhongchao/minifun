2e7660cc46f38ad7f77c0ae0225919534e07a317
mazhongchao <mazhongchao@gmail.com>
2023-01-18 15:03:20 +0800

    enhance class MFConsole

--- src/console/MFConsole.php
+++ src/console/MFConsole.php
@@ -21,19 +21,37 @@ class MFConsole
         'purple'    => ['style'=>'0', 'color'=>'35'],
         'cyan'      => ['style'=>'0', 'color'=>'36'],
         'white'     => ['style'=>'0', 'color'=>'37'],
-        'gray'      => ['style'=>'0', 'color'=>'90'],
+        'gray'      => ['style'=>'0', 'color'=>'90']
     ];
     private static $text_types = [
-        'info'    => ['color'=>'#7a7a7a', 'bg_color'=>'#ededed'],
-        'notice'  => ['color'=>'#0486b3', 'bg_color'=>'#b1e0f0'],
-        'warning' => ['color'=>'#ed8c05', 'bg_color'=>'#fcfbca'],
-        'error'   => ['color'=>'#d12c31', 'bg_color'=>'#ffbdbf'],
-        'success' => ['color'=>'#039c46', 'bg_color'=>'#cafce0'],
-        'black'   => ['color'=>'#555555', 'bg_color'=>'#000000'],
-        'blue'    => ['color'=>'#4175f5', 'bg_color'=>'#000000'],
-        'green'   => ['color'=>'#1e9c02', 'bg_color'=>'#000000'],
-        'red'     => ['color'=>'#d12c31', 'bg_color'=>'#000000'],
-        'yellow'  => ['color'=>'#f58b41', 'bg_color'=>'#000000'],
+        'emergency' => ['color'=>'#ffffff', 'background'=>'#d12c31'],
+        'alert'     => ['color'=>'#000000', 'background'=>'#ffff00'],
+        'critical'  => ['color'=>'#ffffff', 'background'=>'#002eff'],
+        'error'   => ['color'=>'#d12c31', 'background'=>'#ffbdbf'],
+        'warning' => ['color'=>'#ed8c05', 'background'=>'#fcfbca'],
+        'notice'  => ['color'=>'#0486b3', 'background'=>'#b1e0f0'],
+        'info'    => ['color'=>'#7a7a7a', 'background'=>'#ededed'],
+        'debug'   => ['color'=>'#4a86e8', 'background'=>''],
+        'success' => ['color'=>'#039c46', 'background'=>'#cafce0'],
+        'common'  => ['color'=>'#333333', 'background'=>'#ededed'],
+        'black'   => ['color'=>'#222222', 'background'=>''],
+        'red'     => ['color'=>'#ff0000', 'background'=>''],
+        'green'   => ['color'=>'#09a604', 'background'=>''],
+        'yellow'  => ['color'=>'#ff9900', 'background'=>''],
+        'blue'    => ['color'=>'#0000ff', 'background'=>''],
+        'purple'  => ['color'=>'#a635f1', 'background'=>''],
+        'cyan'    => ['color'=>'#04bac7', 'background'=>''],
+        'white'   => ['color'=>'#ffffff', 'background'=>''],
+        'gray'    => ['color'=>'#888888', 'background'=>''],
+        'black-d'   => ['color'=>'#666666', 'background'=>'#000000'],
+        'red-d'     => ['color'=>'#d12c31', 'background'=>'#000000'],
+        'green-d'   => ['color'=>'#1e9c02', 'background'=>'#000000'],
+        'yellow-d'  => ['color'=>'#ffff00', 'background'=>'#000000'],
+        'blue-d'    => ['color'=>'#4175f5', 'background'=>'#000000'],
+        'purple-d'  => ['color'=>'#a635f1', 'background'=>'#000000'],
+        'cyan-d'    => ['color'=>'#00d7ff', 'background'=>'#000000'],
+        'white-d'   => ['color'=>'#ffffff', 'background'=>'#000000'],
+        'gray-d'    => ['color'=>'#aaaaaa', 'background'=>'#000000']
     ];
 
     private static $instance;
@@ -77,9 +95,9 @@ class MFConsole
         if (is_string($text)) {
             if (!empty($type) && isset(self::$text_types[$type])) {
                 $color = self::$text_types[$type]['color'];
-                $background = self::$text_types[$type]['bg_color'];
+                $background = self::$text_types[$type]['background'];
                 if (empty($class)) {
-                    echo "<div style=\"color: $color; background: $background; float:left; width: 80%;\"><pre>{$pre_text}{$text}</pre></div>";
+                    echo "<div style=\"color: $color; background: $background; float:left; width: 80%; padding: 0 10px 0 10px;\"><pre>{$pre_text}{$text}</pre></div>";
                 }
                 else{
                     echo "<div style=\"$class\"><pre>{$pre_text}{$text}</pre></div>";
@@ -87,7 +105,7 @@ class MFConsole
             }
             else {
                 if (empty($class)) {
-                    echo "<div style=\"color: #333; background: #ededed; float:left; width: 80%;\"><pre>{$pre_text}{$text}</pre></div>";
+                    echo "<div style=\"color: #333; float:left; width: 80%; padding: 0 10px 0 10px;\"><pre>{$pre_text}{$text}</pre></div>";
                 }
                 else{
                     echo "<div style=\"$class\"><pre>{$pre_text}{$text}</pre></div>";
@@ -96,7 +114,7 @@ class MFConsole
         }
         else {
             if (empty($class)) {
-                echo "<div style=\"color: #555; background: #ddd; float:left; width: 80%;\">";
+                echo "<div style=\"color: #666; background: #ddd; float:left; width: 80%; padding: 0 10px 0 10px;\">";
             }
             else {
                 echo "<div style=\"$class\">";
