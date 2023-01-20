d0bd8a6460f7c2a50a577655fce8e2b9f5d42f08
mazhongchao <mazhongchao@gmail.com>
2023-01-20 18:06:13 +0800

    enhance MFTempalteTest

--- test/MFTemplateTest.php
+++ test/MFTemplateTest.php
@@ -1,7 +1,14 @@
 <?php
 require "../vendor/autoload.php";
 
-$data = ['title'=>"MFTemplate Test",'content'=>"MiniFun is a PHP programming utilities set"];
+$data = [
+    'title'=>"MFTemplate Test",
+    'content'=>"MiniFun is a PHP programming utilities set",
+    'list'=>[['company1', '$147', '$1,325', '$683', '$524'],
+            ['company2', '$135', '$2,342', '$683', '$524'],
+            ['company3', '$164', '$332', '$331', '$438']],
+    'capitals'=>['China'=>'Peking', 'Germany'=>'Berlin']
+];
 
 MFTemplate::setPath(dirname(__FILE__).DIRECTORY_SEPARATOR."test-resource");
 MFTemplate::render("template-test.html", $data);
