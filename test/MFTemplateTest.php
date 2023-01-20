35be5fb0b2c1b9e2a3bc775b30d3febf8a05e23c
mazhongchao <mazhongchao@gmail.com>
2023-01-20 18:10:05 +0800

    add new MFTemplateTest test file: template-test.php

--- test/MFTemplateTest.php
+++ test/MFTemplateTest.php
@@ -3,12 +3,13 @@ require "../vendor/autoload.php";
 
 $data = [
     'title'=>"MFTemplate Test",
-    'content'=>"MiniFun is a PHP programming utilities set",
+    'content'=>"MiniFun is a PHP programming utilities set.",
     'list'=>[['company1', '$147', '$1,325', '$683', '$524'],
             ['company2', '$135', '$2,342', '$683', '$524'],
             ['company3', '$164', '$332', '$331', '$438']],
-    'capitals'=>['China'=>'Peking', 'Germany'=>'Berlin']
+    'capitals'=>['China'=>'Peking', 'Germany'=>'Berlin', 'Italy'=>'Rome']
 ];
 
 MFTemplate::setPath(dirname(__FILE__).DIRECTORY_SEPARATOR."test-resource");
 MFTemplate::render("template-test.html", $data);
+//MFTemplate::render("template-test.php", $data);
